366bacf0ff4aef495d162317458b744d2cace5b4
Ramky666 <1995stels2009@mail.ru>
2020-04-20 20:46:07 +0300

    new changes

--- ArchiProject/Controllers/HomeController.cs
+++ ArchiProject/Controllers/HomeController.cs
@@ -4,13 +4,10 @@ using System.IO;
 using System.Linq;
 using ArchiProject.Models;
 
-using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-using System.ComponentModel;
-using Ionic.Zip;
-using ZipFile = Ionic.Zip.ZipFile;
+using System.IO.Compression;
 
 namespace ArchiProject.Controllers
 {
@@ -34,69 +31,71 @@ namespace ArchiProject.Controllers
 
         public ActionResult Index(List<ZipModel> files)
         {
+           List<string> filename = files.Where(m => m.Selected == true).Select(f => f.Name).ToList();
 
-            // кароче, это почти работает, как надо :D
+         // я знаю, что этого не достаточно
 
-            using (ZipFile zip = new ZipFile()) // Создаем объект для работы с архивом
+            var filenames = Directory.GetFiles(@"D:/FilesforArchiving");
+            var zipFile = @"D:/FilesforArchiving/Comprest.zip";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
             {
-                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.BestCompression; // MAX степень сжатия
-                zip.AddDirectory("D:/FilesforArchiving"); // Кладем в архив папку вместе с содежимым
-                 zip.Save(@"D:/FilesforArchiving/Comprest/Zip.zip");  // Создаем архив  
+                foreach (var fPath in filenames)
+                {
+                    archive.CreateEntryFromFile(fPath, Path.GetFileName(fPath));
+                }
             }
-        
+
+
+
             return View();
-             
+
         }
 
+//        // отростированный список 
+//        List<string> filenames = files.Where(m => m.Selected == true).Select(f => f.Name).ToList();
+
+//        // дергаетс помощью Guid 
+//        string filename = Guid.NewGuid().ToString() + ".zip";
+
+//        // так, как я понял, выделяется память потока и второй строкой туда запиливается  сжатый файлик
+//        MemoryStream outputMemStream = new MemoryStream();
+//        ZipOutputStream zipStream = new ZipOutputStream(outputMemStream);
+
+//        zipStream.SetLevel(3); // уровень сжатия от 0 до 9
+
+//            // в цикле все тоже самое, что и в закоменчином коде, но тут работает, ну почти(
+//            foreach (string file in filenames)
+//            {
+//                // я понял, что проблема с загрузкой файлов в этой строке, ибо она не видит их в папке
+
+//                FileInfo fi = new FileInfo("~D/FilesforArchiving/Comprest" + file);
+
+//        string entryName = ZipEntry.CleanName(fi.Name);
+//        ZipEntry newEntry = new ZipEntry(entryName);
+//        newEntry.DateTime = fi.LastWriteTime;
+//                newEntry.Size = fi.Length;
+//                zipStream.PutNextEntry(newEntry);
+
+//                byte[] buffer = new byte[4096];
+//                using (FileStream streamReader = System.IO.File.OpenRead(fi.FullName))
+//                {
+//                    StreamUtils.Copy(streamReader, zipStream, buffer);
+//                }
+//zipStream.CloseEntry();
+//            }
+//            zipStream.IsStreamOwner = false;
+//            zipStream.Close();
+
+//            outputMemStream.Position = 0;
+
+//            string file_type = "application/zip";
+//            return File(outputMemStream, file_type, filename);
 
 
 
 
-
-        //    // отростированный список 
-        //    List<string> filenames = files.Where(m => m.Selected == true).Select(f => f.Name).ToList();
-
-        //    // дергаетс помощью Guid 
-        //    string filename = Guid.NewGuid().ToString() + ".zip";
-
-        //    // так, как я понял, выделяется память потока и второй строкой туда запиливается  сжатый файлик
-        //    MemoryStream outputMemStream = new MemoryStream();
-        //    ZipOutputStream zipStream = new ZipOutputStream(outputMemStream);
-
-        //    zipStream.SetLevel(3); // уровень сжатия от 0 до 9
-
-        //    // в цикле все тоже самое, что и в закоменчином коде, но тут работает, ну почти(
-        //    foreach (string file in filenames)
-        //    {
-        //        // я понял, что проблема с загрузкой файлов в этой строке, ибо она не видит их в папке
-
-        //        FileInfo fi = new FileInfo("~D/FilesforArchiving/Comprest" + file);
-
-        //        string entryName = ZipEntry.CleanName(fi.Name);
-        //        ZipEntry newEntry = new ZipEntry(entryName);
-        //        newEntry.DateTime = fi.LastWriteTime;
-        //        newEntry.Size = fi.Length;
-        //        zipStream.PutNextEntry(newEntry);
-
-        //        byte[] buffer = new byte[4096];
-        //        using (FileStream streamReader = System.IO.File.OpenRead(fi.FullName))
-        //        {
-        //            StreamUtils.Copy(streamReader, zipStream, buffer);
-        //        }
-        //        zipStream.CloseEntry();
-        //    }
-        //    zipStream.IsStreamOwner = false;
-        //    zipStream.Close();
-
-        //    outputMemStream.Position = 0;
-
-        //    string file_type = "application/zip";
-        //    return File(outputMemStream, file_type, filename);
-        //}
-
-
-        
-
-
+             
     }
+
 } 
